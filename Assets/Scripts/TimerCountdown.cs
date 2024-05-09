607a4e31209e32613879faf95638e78ebecf864e
Alireza Bahremand <abahrema@asu.edu>
2024-05-08 18:16:34 -0700

    countdown timer + gesture recognition

--- Assets/Scripts/TimerCountdown.cs
+++ Assets/Scripts/TimerCountdown.cs
@@ -1,3 +1,4 @@
+using Oculus.Interaction.Samples;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,17 +7,19 @@ using UnityEngine;
 public class TimerCountdown : MonoBehaviour
 {
     public GameObject root;
-    public TextMeshProUGUI textMesh;
+    public TextMeshProUGUI timerText;
 
-    public int minutes = 5;
-    public int seconds = 0;
+    //public int minutes = 5;
+    //public int seconds = 0;
     public bool timeSet;
-    private float countdown;
+    private float countdownTime;
+    private int secondsOnesPlace;
+    private int secondsTenthsPlace;
 
     private void Start()
     {
-        if (textMesh == null) textMesh = GetComponent<TextMeshProUGUI>();
-        countdown = minutes * 60 + seconds;
+        if (timerText == null) timerText = GetComponent<TextMeshProUGUI>();
+        //countdownTime = minutes * 60 + seconds;
     }
 
     public void SetParent() => root.transform.parent = null;
@@ -24,27 +27,76 @@ public class TimerCountdown : MonoBehaviour
 
     public void SetTimer(float targetTime)
     {
-        textMesh.text = targetTime.ToString();
+        timerText.text = targetTime.ToString();
     }
 
-    public void SetMinutes(int min) => minutes = min;
-    public void SetSeconds(int sec) => seconds = sec;
+    // Method to increase minutes
+    public void IncreaseMinutes()
+    {
+        countdownTime += 60f; // Increase by 1 minute (60 seconds)
+        UpdateTimerText();
+    }
+
+    // Method to increase tenths of seconds
+    public void IncreaseTenthsSeconds()
+    {
+        countdownTime += 10f; // Increase by 0.1 seconds
+        UpdateTimerText();
+    }
+
+    // Method to increase ones place seconds
+    public void IncreaseOnesSeconds()
+    {
+        countdownTime += 1f; // Increase by 1 second
+        UpdateTimerText();
+    }
+
+    // Method to increase minutes
+    public void DecreaseMinutes()
+    {
+        countdownTime -= 60f; // Increase by 1 minute (60 seconds)
+        UpdateTimerText();
+    }
+
+    // Method to increase tenths of seconds
+    public void DecreaseTenthsSeconds()
+    {
+        countdownTime -= 10f; // Increase by 0.1 seconds
+        UpdateTimerText();
+    }
+
+    // Method to increase ones place seconds
+    public void DecreaseOnesSeconds()
+    {
+        countdownTime -= 1f; // Increase by 1 second
+        UpdateTimerText();
+    }
+
+    // Update timer text to display the current countdown time
+    void UpdateTimerText()
+    {
+        int minutes = Mathf.FloorToInt(countdownTime / 60);
+        int seconds = Mathf.FloorToInt(countdownTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+    //public void SetMinutes(int min) => minutes = min;
+    //public void SetSeconds(int sec) => seconds = sec;
 
     void Update()
     {
         if (!timeSet) return;
         // Decrease countdown by deltaTime
-        countdown -= Time.deltaTime;
+        countdownTime -= Time.deltaTime;
 
         // Calculate remaining minutes and seconds
-        int remainingMinutes = Mathf.FloorToInt(countdown / 60);
-        int remainingSeconds = Mathf.FloorToInt(countdown % 60);
+        int remainingMinutes = Mathf.FloorToInt(countdownTime / 60);
+        int remainingSeconds = Mathf.FloorToInt(countdownTime % 60);
 
         // Update text
-        textMesh.text = string.Format("{0:00}:{1:00}", remainingMinutes, remainingSeconds);
+        timerText.text = string.Format("{0:00}:{1:00}", remainingMinutes, remainingSeconds);
 
         // Check if countdown is finished
-        if (countdown <= 0)
+        if (countdownTime <= 0)
         {
             // Do something when timer reaches 0
             Debug.Log("Timer Finished!");
