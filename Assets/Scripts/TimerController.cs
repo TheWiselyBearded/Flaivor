a5e00d3c2028aee6d387e4abd8b0a885f2367299
Alireza Bahremand <abahrema@asu.edu>
2024-05-08 17:05:56 -0700

    wristwatch timer controls

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -15,73 +15,66 @@ public class TimerController : MonoBehaviour
     public ActiveStateSelector leftPose;
     public ActiveStateSelector rightPose;
 
-
-    public TextMeshProUGUI textMesh;
-
-    public int minutes = 5;
-    public int seconds = 0;
-    public bool timeSet;
-
-    private float countdown;
+    public GameObject PFB_Timer;
+    public GameObject leftHandWatchAnchor;
+    public GameObject rightHandWatchAnchor;
 
     void Start()
     {
-        if (textMesh  == null) textMesh = GetComponent<TextMeshProUGUI>();
-        countdown = minutes * 60 + seconds;
-
         leftPose.WhenSelected += LeftPose_WhenSelected;
         leftPose.WhenUnselected += LeftPose_WhenUnselected;
         rightPose.WhenSelected += RightPose_WhenSelected;
         rightPose.WhenUnselected += RightPose_WhenUnselected;
     }
 
+    private void OnDestroy()
+    {
+        leftPose.WhenSelected -= LeftPose_WhenSelected;
+        leftPose.WhenUnselected -= LeftPose_WhenUnselected;
+        rightPose.WhenSelected -= RightPose_WhenSelected;
+        rightPose.WhenUnselected -= RightPose_WhenUnselected;
+    }
+
     private void RightPose_WhenUnselected()
     {
         rightHandUnselectEvent?.Invoke();
+        DeleteTimer(1);
     }
+
 
     private void RightPose_WhenSelected()
     {
         rightHandSelectEvent?.Invoke();
+        CreateTimer(1);
     }
+
 
     private void LeftPose_WhenUnselected()
     {
-        leftHandUnselectEvent?.Invoke(); 
+        leftHandUnselectEvent?.Invoke();
+        DeleteTimer(0);
     }
+
 
     private void LeftPose_WhenSelected()
     {
         leftHandSelectEvent?.Invoke();
+        CreateTimer(0);
+    }
+    
+
+    public void CreateTimer(int handSign)
+    {
+        _ = Instantiate(PFB_Timer, handSign == 0 ? leftHandWatchAnchor.transform : rightHandWatchAnchor.transform);
     }
 
-    public void SetTimer(float targetTime)
+    public void DeleteTimer(int handSign)
     {
-        textMesh.text = targetTime.ToString();
-    }
-
-    public void SetMinutes(int min) => minutes = min;
-    public void SetSeconds(int sec) => seconds = sec;
-
-    void Update()
-    {
-        if (!timeSet) return;
-        // Decrease countdown by deltaTime
-        countdown -= Time.deltaTime;
-
-        // Calculate remaining minutes and seconds
-        int remainingMinutes = Mathf.FloorToInt(countdown / 60);
-        int remainingSeconds = Mathf.FloorToInt(countdown % 60);
-
-        // Update text
-        textMesh.text = string.Format("{0:00}:{1:00}", remainingMinutes, remainingSeconds);
-
-        // Check if countdown is finished
-        if (countdown <= 0)
+        foreach (Transform child in handSign == 0 ? leftHandWatchAnchor.transform : rightHandWatchAnchor.transform)
         {
-            // Do something when timer reaches 0
-            Debug.Log("Timer Finished!");
-            enabled = false; // Disable this script to stop updating the timer
+            Destroy(child.gameObject);
         }
     }
+
+    
 }
